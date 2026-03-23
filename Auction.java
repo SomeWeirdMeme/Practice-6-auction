05121584b0332ae59e5478108992615fdde86bf4
SomeWeirdMeme <ant.osuant@gmail.com>
2026-03-22 23:21:41 -0400

    Initial sharing of project

---
+++ Auction.java
@@ -0,0 +1,132 @@
+import java.util.ArrayList;
+
+/**
+ * A simple model of an auction.
+ * The auction maintains a list of lots of arbitrary length.
+ *
+ * @author David J. Barnes and Michael Kölling.
+ * @version 7.0
+ */
+public class Auction
+{
+    // The list of Lots in this auction.
+    private ArrayList<Lot> listOfLots;
+    // The number that will be given to the next lot entered into this auction.
+    private int nextLotNumber;
+
+    /**
+     * Create a new auction.
+     */
+    public Auction()
+    {
+        listOfLots = new ArrayList<>();
+        nextLotNumber = 1;
+    }
+
+    /**
+     * Enter a new lot into the auction.
+     * @param description A description of the lot.
+     */
+    public void enterLot(String description)
+    {
+        listOfLots.add(new Lot(nextLotNumber, description));
+        nextLotNumber++;
+    }
+
+    /**
+     * Show the full list of lots in this auction.
+     */
+    public void showLots()
+    {
+        for(Lot f : listOfLots) {
+            System.out.println(f.toString());
+        }
+    }
+    
+    /**
+     * Make a bid for a lot.
+     * A message is printed indicating whether the bid is successful or not.
+     * 
+     * @param lotNumber The lot being bid for.
+     * @param bidder The person bidding for the lot.
+     * @param value  The value of the bid.
+     */
+    public void makeABid(int lotNumber, Person bidder, long value)
+    {
+        Lot selectedLot = getLot(lotNumber);
+        if(selectedLot != null) {
+            boolean successful = selectedLot.bidFor(new Balls(bidder, value));
+            if(successful) {
+                System.out.println("The bid for lot number " +
+                                   lotNumber + " was successful.");
+            }
+            else {
+                // Report which bid is higher.
+                Balls highestBid = selectedLot.getHighestBid();
+                System.out.println("Lot number: " + lotNumber +
+                                   " already has a bid of: " +
+                                   highestBid.getValue());
+            }
+        }
+    }
+    
+    /**
+     * Return the lot with the given number. Return null if a lot with this 
+     * number does not exist.
+     * @param lotNumber The number of the lot to return.
+     * @return The lot with the given number, or null.
+     */
+    public Lot getLot(int lotNumber)
+    {
+        if((lotNumber >= 1) && (lotNumber < nextLotNumber)) {
+            // The number seems to be reasonable.
+            Lot selectedLot = listOfLots.get(lotNumber - 1);
+            // Include a confidence check to be sure we have the
+            // right lot.
+            if(selectedLot.getNumber() != lotNumber) {
+                System.out.println("Internal error: Lot number " +
+                                   selectedLot.getNumber() +
+                                   " was returned instead of " +
+                                   lotNumber);
+                // Don't return an invalid lot.
+                selectedLot = null;
+            }
+            return selectedLot;
+        }
+        else {
+            System.out.println("Lot number: " + lotNumber +
+                               " does not exist.");
+            return null;
+        }
+    }
+    
+    public void closeLots(){
+        for(Lot f : listOfLots){
+            Balls erf = f.getHighestBid();
+            
+            if(erf != null){
+                System.out.println("Lot " + f.getNumber() + " :  " + f.getDescription() + " Sold to the poor guy " + erf.getBidder().getName() + " fo " + erf.getValue());
+                
+            } else{
+                System.out.println( "Lot " + f.getNumber() + ": " + f.getDescription() + " naw" );
+            }
+        }
+    }
+    
+    public ArrayList<Lot> getUnsold(){
+        ArrayList<Lot> unsold = new ArrayList<>();
+        int soldCounting = 0;
+        for(Lot t : listOfLots){
+            Balls y = t.getHighestBid();
+            
+            if(y == null){
+                unsold.add(t);
+            } else{
+                soldCounting++;
+            }
+            System.out.println("About " + t.getNumber() + " Are sold, Sorry man");
+            }
+            return unsold;
+    }
+}
+
